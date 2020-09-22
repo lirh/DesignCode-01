e1ecd79cb2505d08dcae9b240e9f1cf5534715e1
lirh <li.rh@icloud.com>
2020-09-23 01:27:12 +0800

    lirh
    LongPressGesture 长按动作处理，根据状态切换背景及阴影并延时恢复，动画

--- DesignCode2/Buttons.swift
+++ DesignCode2/Buttons.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct Buttons: View {
+    
+    @State var tap = false
+    @State var press = false
+    
     var body: some View {
         VStack {
             Text("Button")
@@ -15,10 +19,10 @@ struct Buttons: View {
                 .frame(width: 200, height: 60)
                 .background(
                     ZStack {
-                        Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
+                        Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
                         
                         RoundedRectangle(cornerRadius: 16, style: .continuous)
-                            .foregroundColor(.white)
+                            .foregroundColor(Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             .blur(radius: 4)
                             .offset(x: -8, y: -8)
                         
@@ -31,12 +35,28 @@ struct Buttons: View {
                     }
                 )
                 .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
-                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+                .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                .shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+                .scaleEffect(tap ? 1.2 : 1)
+                .gesture(
+                    //最小间隔、最大间隔
+                    LongPressGesture(minimumDuration: 0.5, maximumDistance: 10).onChanged({ value in
+                        self.tap = true
+                        //增加延时 恢复
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            self.tap = false
+                        }
+                    })
+                    .onEnded({ value in
+                        self.press.toggle()
+                    })
+                    
+                )
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
         .edgesIgnoringSafeArea(.all)
+        .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
     }
 }
 
