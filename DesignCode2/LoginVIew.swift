ac7bf7e9009ee3a2df6f99def5fe3f6bad3c28b0
lirh <li.rh@icloud.com>
2020-09-24 02:15:31 +0800

    lirh
    1、.scaleEffect 和 .animation 放到 .rotation3DEffect前面，解决当拖拽最后掉帧的情况
    2、根据viewState: CGsize 制作文字和背景图的偏移效果

--- DesignCode2/LoginVIew.swift
+++ DesignCode2/LoginVIew.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct LoginVIew: View {
     @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -26,10 +28,12 @@ struct LoginVIew: View {
                 }
                 .frame(maxWidth: .infinity,maxHeight: 100)
                 .padding(.horizontal, 16)
+                .offset(x: viewState.width / 15, y: viewState.height / 15)
                 
                 Text("80 hours of courses for SwiftUI, React and design tools.")
                     .font(.subheadline)
                     .frame(width: 250)
+                    .offset(x: viewState.width / 20, y: viewState.height / 20)
                 
                 Spacer()
             }
@@ -57,10 +61,26 @@ struct LoginVIew: View {
                         .blendMode(.overlay)
                 }
             )
-            
-            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")), alignment: .bottom)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3")).offset(x: viewState.width / 15, y: viewState.height / 15),
+                alignment: .bottom
+            )
             .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            //缩放
+            .scaleEffect(isDragging ? 0.9 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: viewState.width, y: viewState.height, z: 0))
+            
+            .gesture(
+                DragGesture().onChanged({ value in
+                    viewState = value.translation
+                    isDragging = true
+                }).onEnded({ value in
+                    viewState = .zero
+                    isDragging = false
+                })
+            )
         }
     }
 }
