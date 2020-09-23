ec5cd11a2aa9123025174f54aae5c13f2d346058
lirh <li.rh@icloud.com>
2020-09-23 21:21:10 +0800

    lirh
    LoginView的顶部布局及背景

--- DesignCode2/LoginVIew.swift
+++ DesignCode2/LoginVIew.swift
@@ -9,12 +9,49 @@ import SwiftUI
 
 struct LoginVIew: View {
     var body: some View {
-        GeometryReader { geometry in
-            Text("Learn design & code. \nFrom scratch")
-                .font(.system(size: geometry.size.width / 10, weight: .bold))
+        ZStack(alignment: .top) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            Color("background2")
+                .edgesIgnoringSafeArea(.bottom)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            
+            VStack {
+                GeometryReader { geometry in
+                    Text("Learn design & code. \nFrom scratch")
+                        .font(.system(size: geometry.size.width / 10, weight: .bold))
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity,maxHeight: 100)
+                .padding(.horizontal, 16)
+                
+                Text("80 hours of courses for SwiftUI, React and design tools.")
+                    .font(.subheadline)
+                    .frame(width: 250)
+                
+                Spacer()
+            }
+            //多行文本居中对齐
+            .multilineTextAlignment(.center)
+            .padding(.top, 100)
+            .frame(height: 477)
+            .frame(maxWidth: .infinity)
+            .background(
+                //重叠透明图片，不同光照制作背景
+                ZStack {
+                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                        .offset(x:-150, y: -200)
+                        .blendMode(.plusDarker)
+                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                        .offset(x:-200, y: -250)
+                        .blendMode(.overlay)
+                }
+            )
+            
+            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")), alignment: .bottom)
+            .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
         }
-        .frame(maxWidth: .infinity)
-        .padding(.horizontal, 16)
     }
 }
 
